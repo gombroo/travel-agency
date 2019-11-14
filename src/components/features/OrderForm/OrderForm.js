7940a9fd6abec22a19cfb27c1996d2437f1c0bc8
Ola Siem <info@siemieniuk.net>
2019-11-14 14:22:40 +0100

    Add container for OrderForm

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Row, Col} from 'react-flexbox-grid';
+//import styles from './OrderForm.scss';
 import OrderSummary from '../OrderSummary/OrderSummary';
 
-const OrderForm = tripCost => (
-
+const OrderForm = (tripCost, options) => (
   <Row>
     <Col xs={12}>
-      <OrderSummary tripCost={tripCost} />
+      <OrderSummary options={options} tripCost={tripCost} />
     </Col>
   </Row>
 );
 
 OrderSummary.PropTypes = {
   tripCost: PropTypes.string,
+  options: PropTypes.node,
 };
 
 export default OrderForm;
