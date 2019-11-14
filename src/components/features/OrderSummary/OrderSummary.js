880aa1b561e71276de01a190be8ee8faeece715f
Ola Siem <info@siemieniuk.net>
2019-11-14 16:43:50 +0100

    Start calculate total price in OrderSummary

--- src/components/features/OrderSummary/OrderSummary.js
+++ src/components/features/OrderSummary/OrderSummary.js
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './OrderSummary.scss';
 
+//import {calculateTotal} from '../../../utils/calculateTotal';
+//import {formatPrice} from '../../../utils/formatPrice';
+
 const OrderSummary = props => (
   <h2 className={styles.component}>
     Total: <strong>{props.tripCosts}</strong>
+    {/*Total: <strong>{calculateTotal(formatPrice(props.tripCosts), props.options)}</strong>*/}
   </h2>
 );
 
 OrderSummary.propTypes ={
   tripCosts: PropTypes.node,
+  options: PropTypes.node,
 };
 
 export default OrderSummary;
