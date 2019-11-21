6db8b2277441b3690deb5a0ca129c955fb67f59f
Ola Siem <info@siemieniuk.net>
2019-11-21 10:48:16 +0100

    Add order option checkboxes

--- src/components/features/OrderOption/OrderOptionCheckboxes.js
+++ src/components/features/OrderOption/OrderOptionCheckboxes.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './OrderOption';
+import { formatPrice } from '../../../utils/formatPrice';
 
+const newValueSet = (currentValue, id, checked) => {
+  if (checked) {
+    return [...currentValue, id];
+  } else {
+    return currentValue.filter(value => value != id);
+  }
+};
 
-const OrderOptionCheckboxes = props => (
-  <div>{props.name}</div>
+const OrderOptionCheckboxes = ({values, currentValue, setOptionValue}) => (
+  <div className={styles.checkboxes}>
+    {values.map(value => (
+      <label key={value.id}>
+        <input
+          type="checkbox"
+          value={value.id}
+          checked={currentValue[value.id]}
+          onChange={event => setOptionValue(newValueSet(currentValue, value.id, event.currentTarget.checked))}/>
+        {value.name} ({formatPrice(value.price)})
+        <br/>
+      </label>
+    ))}
+  </div>
 );
 
 OrderOptionCheckboxes.propTypes = {
-  name: PropTypes.string,
+  values: PropTypes.array,
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.array,
 };
 
 export default OrderOptionCheckboxes;
