7076ad6c2faf083d650f05654407674b9ddcd6dd
Ola Siem <info@siemieniuk.net>
2019-11-28 18:27:04 +0100

    Send order to API

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import { Row, Col } from 'react-flexbox-grid';
+import { formatPrice } from '../../../utils/formatPrice';
+import { calculateTotal } from '../../../utils/calculateTotal';
+
 import OrderSummary from '../OrderSummary/OrderSummary';
-import pricing from '../../../data/pricing';
+import pricing from '../../../data/pricing.json';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
+import settings from '../../../data/settings';
+
+const sendOrder = (options, tripCost) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+
+  const payload = {
+    ...options,
+    totalCost,
+  };
+
+  const url = settings.db.url + '/' + settings.db.endpoint.orders;
+
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
 
 const OrderForm = props => (
   <Row>
@@ -17,6 +50,11 @@ const OrderForm = props => (
     ))}
     <Col xs={12}>
       <OrderSummary tripCost={props.tripCost} options={props.options} />
+      <Button
+        onClick={() =>
+          sendOrder(props.options, props.tripCost, props.tripId, props.countryCode, props.tripName)}>
+        Order now!
+      </Button>
     </Col>
   </Row>
 );
@@ -25,6 +63,9 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func,
+  tripId: PropTypes.number,
+  countryCode: PropTypes.object,
+  tripName: PropTypes.string,
 };
 
 export default OrderForm;
