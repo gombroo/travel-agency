ca7b1870fd89aa376a6083f6fdb14549ac1fc567
Ola Siem <info@siemieniuk.net>
2019-12-06 08:20:47 +0100

    Fix promoPrice counting

--- src/utils/promoPrice.js
+++ src/utils/promoPrice.js
@@ -1,3 +1,8 @@
+// export const promoPrice = (cost, per) => {
+// return cost - (cost /100) * per;
+// };
+
 export const promoPrice = (cost, per) => {
-  return cost - (cost /100) * per;
+  let price = cost.replace('$', '').replace(',', '');
+  return price - (price / 100) * per;
 };
